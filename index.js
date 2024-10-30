d84428911142d05b7b64fae4343925d4d53ad59d
niklauskus <niklauskus.server01@gmail.com>
2024-10-31 00:03:27 +0300

    Исправление Webhook URL

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Подключаем dotenv в самом начале
+require('dotenv').config();
 
 const express = require('express');
 const { Telegraf } = require('telegraf');
@@ -12,7 +12,7 @@ app.use(bot.webhookCallback('/webhook'));
 // Запуск сервера и установка Webhook
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
-  const webhookUrl = `https://telegram-hello-bot-nmyd.vercel.app/webhook`; // Замените на реальный URL вашего приложения на Vercel
+  const webhookUrl = `https://telegram-hello-bot-nmyd.vercel.app/webhook`;
   try {
     await bot.telegram.setWebhook(webhookUrl);
     console.log(`Webhook установлен на ${webhookUrl}`);
